c0aec1a62e672c9858e9618d1eb952bc370e05c5
AagmanBharti <aagmanbharti01@gmail.com>
2026-01-20 11:34:11 +0530

    Delete NLP directory

--- NLP/spacy_lemma.py
+++
@@ -1,5 +0,0 @@
-import spacy 
-nlp = spacy.load("en_core_web_sm") 
-doc = nlp("The children are running in the playground") 
-lemmas = [token.lemma_ for token in doc] 
-print(lemmas)
--- NLP/streamlit_basics.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-st.title("Hello Streamlit")
-st.write("This is my app")
-st.text_area("This is a text area")
-st.text_input("This is input")  

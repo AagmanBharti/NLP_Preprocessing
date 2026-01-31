6418da6bfa7b196cf42aec9e8abcf5318ad446bc
AagmanBharti <aagmanbharti01@gmail.com>
2026-02-01 02:12:22 +0530

    Refactor load_spacy_model to always load the model
    Simplified the load_spacy_model function by removing error handling and download logic.

--- NLP_PROJECT/project3(word_embedding).py
+++ NLP_PROJECT/project3(word_embedding).py
@@ -28,11 +28,7 @@ load_nltk()
 # =========================
 @st.cache_resource
 def load_spacy_model():
-    try:
-        return spacy.load("en_core_web_sm")
-    except:
-        download("en_core_web_sm")
-        return spacy.load("en_core_web_sm")
+    return spacy.load("en_core_web_sm")
 
 nlp = load_spacy_model()
 
